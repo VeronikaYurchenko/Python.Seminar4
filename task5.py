418570a15d06fd7a0d1bfe6cf7c56913544f5d50
Yurchenko Veronika <vasilieva-nika@yandex.ru>
2022-10-25 16:18:54 +0900

    Task 5

---
+++ task5.py
@@ -0,0 +1,32 @@
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+# Пример двух заданных многочленов:
+# 23x⁹ - 16x⁸ + 3x⁷ + 15x⁴ - 2x³ + x² + 20 = 0
+# 17x⁹ + 15x⁸ - 8x⁷ + 15x⁶ - 10x⁴ + 7x³ - 13x¹ + 33 = 0
+# Результат:
+# 40x⁹ - x⁸ -5x⁷ + 15x⁶ +5x⁴ + 5x³ + x² - 13x¹ + 53 = 0 40x9
+
+with open('poly_1.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^9 - 16x^8 + 3x^7 + 15x^4 - 2x^3 + x^2 + 20')
+with open('poly_2.txt', 'w', encoding='utf-8') as file:
+    file.write('17x^9 + 15x^8 - 8x^7 + 15x^6 - 10x^4 + 7x^3 - 13x^1 + 33')
+
+with open('poly_1.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+
+
+with open('poly_2.txt','r') as file: 
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+
+with open('sum_poly.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
+
+
+
+
+
